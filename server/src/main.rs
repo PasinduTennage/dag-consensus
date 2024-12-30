53d1bf9eec5afadc60e425e70da0da0c0c3e2730
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-12-30 16:15:36 +0100

    adding message types

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use serde_json::Value;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-use utils::ClientRequest;
+use utils::{ClientRequest, Message, ReplicaMessage};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,9 +14,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut buffer = vec![0; 1024];
             if let Ok(size) = socket.read(&mut buffer).await {
                 if size > 0 {
-                    match serde_json::from_slice::<ClientRequest>(&buffer[..size]) {
+                    match serde_json::from_slice::<Message>(&buffer[..size]) {
                         Ok(message) => {
-                            println!("received: {:?}", message);
+                            match message {
+                                Message::ClientRequest(client_request) => {
+                                    println!("received ClientRequest: {:?}", client_request);
+                                }
+                                Message::ReplicaMessage(replica_message) => {
+                                    println!("received ReplicaMessage: {:?}", replica_message);
+                                }
+                            }
                             if let Err(e) = socket.write_all(&buffer[..size]).await {
                                 eprintln!("failed to send response {}", e);
                             }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -4,9 +4,31 @@ use serde::{Serialize, Deserialize};
 
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ClientRequest{
+pub struct ClientRequestBatch {
     pub id: String,
-    pub payload: Vec<u8>,
+    pub payload: Vec<Vec<u8>>,
     pub sender: i32,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ControlMessage {
+    pub operation: i32,
+    pub str_params :Vec<String>,
+    pub int_params :Vec<i32>,
+    pub sender: i32,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ReplicaMessage {
+   pub sender: i32,
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+pub enum Message {
+    ClientRequest(ClientRequestBatch),
+    Control(ControlMessage),
+    ReplicaMessage(ReplicaMessage),
+}
